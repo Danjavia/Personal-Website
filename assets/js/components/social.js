703ca7bf47f70322505093808858cca763943f64
Danny Viasus(Danjavia) <danny.viasus@comparamejor.com>
2015-12-26 01:18:46 -0500

    remove some socials

--- assets/js/components/social.js
+++ assets/js/components/social.js
@@ -9,8 +9,6 @@ var Social = React.createClass({
 			<div className="social">
 				<a href="https://twitter.com/danjavia" className=" social__icon"><i className="fa fa-twitter"></i></a>
 				<a href="https://github.com/danjavia" className=" social__icon"><i className="fa fa-github"></i></a>
-				<a href="https://codepen.io/danjavia" className=" social__icon"><i className="fa fa-codepen"></i></a>
-				<a href="https://medium.com/@Danjavia" className=" social__icon"><i className="fa fa-medium"></i></a>
 				<a href="mailto:d@danjavia.xyz" className=" social__icon"><i className="fa fa-envelope"></i></a>
 			</div>
 		);
